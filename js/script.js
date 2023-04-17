abf93f8d8381cd697c4c04e47098411ce3fd10c1
Yevonnie L <46805387+YL31@users.noreply.github.com>
2023-04-16 22:45:18 -0400

    Add more functionality to guess button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
-const input = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
 const spanRemaining = document.querySelector("span");
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = ["m", "a" ,"g", "n" ,"o", "l", "i" ,"a"];
+const guessedLetters = [];
 
 const placeholder = function (word) {
   const wordArray = [];
@@ -22,11 +23,38 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const guessInput = input.value;
-    console.log(guessInput);
-    input.value = "";
-  });
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+    message.innerText = "";
+    const yourGuess = validateInput(guess);
+    console.log(yourGuess);
+    makeGuess(yourGuess);
+});
+
+const validateInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+
+  if (input.length === 0) {
+    message.innerText = "I don't see anything! You need to type in a letter.";
+  } else if (input.length > 1) {
+    message.innerText = "Please type in one letter.";
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "What's that? Type in a letter between A and Z.";
+  } else {
+    return input;
+  }
+  };
   
-
-
+  const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+      message.innerText =
+        "Wait a minute! You already guessed that letter, so try again.";
+    } else {
+      guessedLetters.push(guess);
+      console.log(guessedLetters);
+    }
+  };
+  
 
